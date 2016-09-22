f46c5a2cc9263283d4013e9dfb6e87a20161222e
Royston <rlqhenson@hotmail.com>
2016-09-22 20:13:21 +0100

    added test cases to 'fizzbuzz_spec.rb'

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,13 +1,13 @@
 require "fizzbuzz"
 
 describe "fizzbuzz" do
-  it "returns 'fizz' when passed a number divisible by 3" do
-    expect(fizzbuzz(3)).to eq "fizz"
+  it "returns 'fizz' when passed a number divisible by 3 AND 5" do
+    expect(fizzbuzz(15)).to eq "fizz"
   end
   it "returns 'buzz' when passed a number divisible by 5" do
     expect(fizzbuzz(5)).to eq "buzz"
   end
-  it "returns 'fizzbuzz' when passed a number divisible by 3 AND 5" do
-    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  it "returns 'fizzbuzz' when passed a number divisible by 3" do
+    expect(fizzbuzz(3)).to eq "fizzbuzz"
   end
 end
