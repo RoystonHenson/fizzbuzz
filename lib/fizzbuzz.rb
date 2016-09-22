53d8ae50a0007d5601f8af903674243713070abd
Royston <rlqhenson@hotmail.com>
2016-09-22 20:12:22 +0100

    added if statement to 'fizzbuzz.rb'

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,12 @@
 def fizzbuzz(number)
-  "fizz"
+  if number % 3 == 0 && number % 5 == 0
+    num = "fizz"
+  elsif number % 5 == 0
+    num = "buzz"
+  elsif number % 3 == 0
+    num = "fizzbuzz"
+  else
+    num = number
+  end
+  return num
 end
